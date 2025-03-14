8f187dd7af5ccfb4bda6d00a8511c3be42702a4d
Mireli Borges <mika.oliveira1998@gmail.com>
2025-03-13 21:10:18 -0300

    Tipo - model, controller e repository

--- Documents/workspace-spring-tool-suite-4-4.27.0.RELEASE/ProjetoCRM/src/main/java/com/generation/CRM/repository/TipoRepository.java
+++ Documents/workspace-spring-tool-suite-4-4.27.0.RELEASE/ProjetoCRM/src/main/java/com/generation/CRM/repository/TipoRepository.java
@@ -1,5 +1,14 @@
 package com.generation.CRM.repository;
 
-public interface TipoRepository {
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
+
+import com.generation.CRM.model.Tipo;
+
+public interface TipoRepository extends JpaRepository<Tipo, Long> {
+
+	public List<Tipo> findAllByNomeContainingIgnoreCase(@Param("nome") String nome);
 
 }
